51361d1b9bf8cf126fa745b7233a0af83a741274
samisamosa <samihadatta@gmail.com>
2020-05-16 13:48:23 -0400

    reverted to correct routing

--- main.js
+++ main.js
@@ -92,6 +92,8 @@ let template = (name, templateFunction) => {
 // when entering that path. A template can be a string (file name), or a function that
 // will directly create the DOM objects.
 let route = (path, template) => {
+    console.log('in route');
+    console.log(path);
     if (typeof template == "function") {
       return routes[path] = template;
     }
@@ -103,36 +105,31 @@ let route = (path, template) => {
     }
 };
 // Register the templates.
-template('home', () => {
-    let myDiv = document.getElementById("home");
+template('template1', () => {
+    let myDiv = document.getElementById(appDiv);
     myDiv.innerHTML = "";
     const link1 = createLink('view1', 'Go to view1', '#/view1');
     const link2 = createLink('view2', 'Go to view2', '#/view2');
     myDiv.appendChild(link1);
     return myDiv.appendChild(link2);
 });
-template('projects', () => {
+template('template-view1', () => {
     let myDiv = document.getElementById(appDiv);
     myDiv.innerHTML = "";
     const link1 = createDiv('view1', "<div><h1>This is View 1 </h1><a href='#/'>Go Back to Index</a></div>");
     return myDiv.appendChild(link1);
 });
-template('interests', () => {
-    let myDiv = document.getElementById(appDiv);
-    myDiv.innerHTML = "";
-    const link2 = createDiv('view2', "<div><h1>This is View 2 </h1><a href='#/'>Go Back to Index</a></div>");
-    return myDiv.appendChild(link2);
-});
-template('contact', () => {
+template('template-view2', () => {
     let myDiv = document.getElementById(appDiv);
     myDiv.innerHTML = "";
     const link2 = createDiv('view2', "<div><h1>This is View 2 </h1><a href='#/'>Go Back to Index</a></div>");
     return myDiv.appendChild(link2);
 });
 // Define the mappings route->template.
-route('/', 'home');
+route('/', 'template1');
 route('/view1', 'template-view1');
 route('/view2', 'template-view2');
+console.log('routes should be defined');
 // Generate DOM tree from a string
 let createDiv = (id, xmlString) => {
     let d = document.createElement('div');
@@ -151,19 +148,25 @@ let createLink = (title, text, href) => {
 };
 
 // Give the correspondent route (template) or fail
-let resolveRoute = (route) => {
+let resolveRoute = (path) => {
+    console.log('in resolveRoute');
+    console.log(path);
     try {
-     return routes[route];
+        console.log('in try');
+        console.log(routes);
+        console.log(routes[path]);
+     return routes[path];
     } catch (error) {
         throw new Error("The route is not defined");
     }
 };
 // The actual router, get the current URL and generate the corresponding template
 let router = (evt) => {
+    console.log('in router');
     const url = window.location.hash.slice(1) || "/";
     const routeResolved = resolveRoute(url);
+    console.log(routeResolved);
     routeResolved();
-    // resolveRoute(url);
 };
 // For first load or when routes are changed in browser url box.
 window.addEventListener('load', router);
